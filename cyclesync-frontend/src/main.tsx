e0c4bb8306e8191f22d82849ce595e48538bdc74
Viella <168560797+unmatched78@users.noreply.github.com>
2025-05-30 13:27:29 +0000

    main

--- cyclesync-frontend/src/main.tsx
+++ cyclesync-frontend/src/main.tsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import App from './App';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Custom Chakra theme
+const theme = extendTheme({
+  colors: {
+    brand: {
+      50: '#FFE6F0',
+      100: '#FFCCE5',
+      500: '#FF69B4',
+      600: '#C2185B',
+    },
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'linear-gradient(145deg, brand.50, white)',
+        color: 'gray.800',
+      },
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
