225a02a637e178b2f1c3cb4b17b57c5282f17ec4
Viella <168560797+unmatched78@users.noreply.github.com>
2025-06-08 20:03:27 +0000

    deleted rest urls

--- mensetrack/urls.py
+++ mensetrack/urls.py
@@ -6,10 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
-    path('api-auth/', include('rest_framework.urls')),  # Optional for browsable API
 ]
-#save stataic  and medai
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,  document_root=settings.MEDIA_ROOT)
