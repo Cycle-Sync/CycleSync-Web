7e54d02c8cb0599d8f6bcf124fc8adda114f2576
Viella <168560797+unmatched78@users.noreply.github.com>
2025-06-08 20:12:05 +0000

    exceptions logic migrated to settings

--- core/apps.py
+++ core/apps.py
@@ -7,12 +7,4 @@ class CoreConfig(AppConfig):
     def ready(self):
         # Import signals to ensure they are registered
         import core.signals
-        # Import custom exception handler
-        from core.exceptions import custom_exception_handler
-        # Register the custom exception handler with Django REST Framework
-        from rest_framework.views import exception_handler
-        exception_handler.custom_exception_handler = custom_exception_handler
-        # Ensure the custom exception handler is used
-        from rest_framework.settings import api_settings
-        api_settings.EXCEPTION_HANDLER = 'core.exceptions.custom_exception_handler'
         
