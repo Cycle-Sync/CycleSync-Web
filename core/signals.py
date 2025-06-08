c30011ef2e4ce4121d5c94d573bde9f8619b5d66
Viella <168560797+unmatched78@users.noreply.github.com>
2025-06-08 20:04:15 +0000

    signals for profile registeration

--- core/signals.py
+++ core/signals.py
@@ -1,6 +1,14 @@
-#core/signals.py
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from django.core.mail import send_mail
-from django.conf import settings
+from .models import Profile
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(
+            user=instance,
+            date_of_birth=timezone.now().date(),   # or null=True on the field
+            country='',
+            cycle_type='unknown',
+        )
