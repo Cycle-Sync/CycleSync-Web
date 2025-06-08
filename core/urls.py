d1e63c274436b76b0f72dae299e8f4919b1036c5
Viella <168560797+unmatched78@users.noreply.github.com>
2025-06-08 19:49:02 +0000

    cleaning

--- core/urls.py
+++ core/urls.py
@@ -1,17 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from core.views import RegisterView, LoginView, LogoutView, ProfileView, DailyEntryView, CalendarView, DashboardView
+from core.views import *
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+# Register your viewsets here if you have any
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/register/', RegisterView.as_view(), name='register'),
-    path('api/login/', LoginView.as_view(), name='login'),
-    path('api/logout/', LogoutView.as_view(), name='logout'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/profile/', ProfileView.as_view(), name='profile'),
-    path('api/daily-entry/', DailyEntryView.as_view(), name='daily_entry'),
-    path('api/calendar/', CalendarView.as_view(), name='calendar'),
-    path('api/dashboard/', DashboardView.as_view(), name='dashboard'),
-    path('api-auth/', include('rest_framework.urls')),  # Optional for browsable API
+    path('register/', RegisterView.as_view(), name='register'),
+    path('auth/token/login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),#for api users
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('daily-entry/', DailyEntryView.as_view(), name='daily_entry'),
+    path('calendar/', CalendarView.as_view(), name='calendar'),
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),
+    path('', include(router.urls)),
 ]
--- mensetrack/urls.py
+++ mensetrack/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
+    path('api/', include('core.urls')),
+    path('api-auth/', include('rest_framework.urls')),  # Optional for browsable API
 ]
 #save stataic  and medai
 
