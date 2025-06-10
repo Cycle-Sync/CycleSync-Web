7f9c603a53aece094ea74a30542411b043204e28
unmatched78 <marvinavi24@gmail.com>
2025-06-10 14:49:15 +0200

    using brnad name

--- sync/src/components/app-sidebar.tsx
+++ sync/src/components/app-sidebar.tsx
@@ -110,7 +110,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             >
               <a href="#">
                 <IconInnerShadowTop className="!size-5" />
-                <span className="text-base font-semibold">Acme Inc.</span>
+                <span className="text-base font-semibold">CycleSync.</span>
               </a>
             </SidebarMenuButton>
           </SidebarMenuItem>
