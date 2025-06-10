cd795017d8c1c675809ad37489e4e564cadfd800
unmatched78 <marvinavi24@gmail.com>
2025-06-10 14:56:32 +0200

    dashabord redirect

--- sync/src/components/app-sidebar.tsx
+++ sync/src/components/app-sidebar.tsx
@@ -108,7 +108,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
               asChild
               className="data-[slot=sidebar-menu-button]:!p-1.5"
             >
-              <a href="/">
+              <a href="/app/dashboard">
                 <IconInnerShadowTop className="!size-5" />
                 <span className="text-base font-semibold">CycleSync.</span>
               </a>
